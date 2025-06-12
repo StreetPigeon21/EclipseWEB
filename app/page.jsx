1a34511859723d56bf4a97fc8051bed7d327c203
StreetPigeon21 <asketr72@gmail.com>
2025-06-12 19:01:08 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,50 +1,45 @@
-import React from 'react';
-import { Card } from './components/card';
-import { ContextAlert } from './components/context-alert';
-import { Markdown } from './components/markdown';
-import { RandomQuote } from './components/random-quote';
-// Replace this with a safe fallback if Netlify context isn't available
-const ctx = process.env.REACT_APP_CONTEXT || 'prod';
+import Link from 'next/link';
+import { Card } from 'components/card';
+import { ContextAlert } from 'components/context-alert';
+import { Markdown } from 'components/markdown';
+import { RandomQuote } from 'components/random-quote';
+import { getNetlifyContext } from 'utils';
 
-const contextExplainer = `
-The card below is rendered based on the value of \`REACT_APP_CONTEXT\`, 
-which you can set in your environment (like .env file or CI build settings).
-`;
+const contextExplainer = 
+The card below is rendered on the server based on the value of \process.env.CONTEXT\ 
+([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
+;
 
-const preDynamicContentExplainer = `
-The card content below is fetched from \`/quotes/random\` with a different quote shown on each page load:
-`;
+const preDynamicContentExplainer = 
+The card content below is fetched by the client-side from \/quotes/random\ (see file \app/quotes/random/route.js\) with a different quote shown on each page load:
+;
 
-const postDynamicContentExplainer = `
-On Netlify, dynamic routes or API endpoints can be hosted as [Serverless Functions](https://docs.netlify.com/functions/overview/).
+const postDynamicContentExplainer = 
+On Netlify, Next.js Route Handlers are automatically deployed as [Serverless Functions](https://docs.netlify.com/functions/overview/).
+Alternatively, you can add Serverless Functions to any site regardless of framework, with acccess to the [full context data](https://docs.netlify.com/functions/api/).
 
 And as always with dynamic content, beware of layout shifts & flicker! (here, we aren't...)
-`;
+;
 
-export default function App() {
+const ctx = getNetlifyContext();
+
+export default function Page() {
     return (
         <div className="flex flex-col gap-12 sm:gap-16">
             <section>
                 <ContextAlert className="mb-6" />
-                <h1 className="mb-4">Netlify Platform Starter - React</h1>
-                <p className="mb-6 text-lg">Get started with React and Netlify in seconds.</p>
-                <a
-                    href="https://docs.netlify.com/frameworks/react/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-lg sm:min-w-64"
-                >
+                <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
+                <p className="mb-6 text-lg">Get started with Next.js and Netlify in seconds.</p>
+                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
                     Read the Docs
-                </a>
+                </Link>
             </section>
-
-            {ctx && (
+            {!!ctx && (
                 <section className="flex flex-col gap-4">
                     <Markdown content={contextExplainer} />
-                    <RuntimeContextCard ctx={ctx} />
+                    <RuntimeContextCard />
                 </section>
             )}
-
             <section className="flex flex-col gap-4">
                 <Markdown content={preDynamicContentExplainer} />
                 <RandomQuote />
@@ -54,19 +49,18 @@ export default function App() {
     );
 }
 
-function RuntimeContextCard({ ctx }) {
-    const title = `Netlify Context: running in ${ctx} mode.`;
-
+function RuntimeContextCard() {
+    const title = Netlify Context: running in ${ctx} mode.;
     if (ctx === 'dev') {
         return (
             <Card title={title}>
-                <p>React will rebuild pages live in development mode.</p>
+                <p>Next.js will rebuild any page you navigate to, including static pages.</p>
             </Card>
         );
     } else {
         return (
             <Card title={title}>
-                <p>This page was statically generated at build time.</p>
+                <p>This page was statically-generated at build time.</p>
             </Card>
         );
     }
